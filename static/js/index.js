d3d7e0ddd4500c3918c7c33e36903c52864390f8
irunika <irunikail@gmail.com>
2016-03-30 17:57:16 +0530

    Initial Commit of real stuff replacing dummy

--- static/js/index.js
+++ static/js/index.js
@@ -12,41 +12,41 @@ $(document).ready( function(){
 
     $('.budget-circle').css({'height': $('.budget-circle').width()});
 
-    if (window.screen.width > 768){
-        if($(window).scrollTop() < 1000){
-            $('.navbar').css({'margin-top':'0px'});
-            $('.navbar').css({'transition':'margin-top 300ms'});
-            $('.navbar').css({'background':'#f7f7f7'});
-            //$('.navbar').css({'border-bottom':'solid 2px'});
-            //$('.navbar').css({'border-color':'black'});
-        }
-        if ($(window).scrollTop() < 500){
-            $('.navbar').css({'margin-top':'50px'});
-            $('.navbar').css({'background':'rgba(255, 255, 255, 0.80)'});
-            //$('.navbar').css({'border-color':'rgba(255, 255, 255, 0.00)'});
-        }
-    }
+    //if (window.screen.width > 768){
+    //    if($(window).scrollTop() < 1000){
+    //        $('.navbar').css({'margin-top':'0px'});
+    //        $('.navbar').css({'transition':'margin-top 300ms'});
+    //        $('.navbar').css({'background':'#f7f7f7'});
+    //        //$('.navbar').css({'border-bottom':'solid 2px'});
+    //        //$('.navbar').css({'border-color':'black'});
+    //    }
+    //    if ($(window).scrollTop() < 500){
+    //        $('.navbar').css({'margin-top':'50px'});
+    //        $('.navbar').css({'background':'rgba(255, 255, 255, 0.80)'});
+    //        //$('.navbar').css({'border-color':'rgba(255, 255, 255, 0.00)'});
+    //    }
+    //}
 
 
-    $(window).scroll(function(e){
-        console.log( window.screen.width);
-        console.log( window.screen.height);
-
-        if (window.screen.width > 768){
-            if($(window).scrollTop() < 900){
-                $('.navbar').css({'margin-top':'0px'});
-                $('.navbar').css({'transition':'margin-top 300ms'});
-                $('.navbar').css({'background':'#f7f7f7'});
-                //$('.navbar').css({'border-bottom':'solid 2px'});
-                //$('.navbar').css({'border-color':'black'});
-            }
-            if ($(window).scrollTop() < 300){
-                $('.navbar').css({'margin-top':'50px'});
-                $('.navbar').css({'background':'rgba(255, 255, 255, 0.80)'});
-                //$('.navbar').css({'border-color':'rgba(255, 255, 255, 0.00)'});
-            }
-        }
-    });
+    //$(window).scroll(function(e){
+    //    console.log( window.screen.width);
+    //    console.log( window.screen.height);
+    //
+    //    if (window.screen.width > 768){
+    //        if($(window).scrollTop() < 900){
+    //            $('.navbar').css({'margin-top':'0px'});
+    //            $('.navbar').css({'transition':'margin-top 300ms'});
+    //            $('.navbar').css({'background':'#f7f7f7'});
+    //            //$('.navbar').css({'border-bottom':'solid 2px'});
+    //            //$('.navbar').css({'border-color':'black'});
+    //        }
+    //        if ($(window).scrollTop() < 300){
+    //            $('.navbar').css({'margin-top':'50px'});
+    //            $('.navbar').css({'background':'rgba(255, 255, 255, 0.80)'});
+    //            //$('.navbar').css({'border-color':'rgba(255, 255, 255, 0.00)'});
+    //        }
+    //    }
+    //});
     
     $(window).resize(function () {
         $('.budget-circle').css({'height': $('.budget-circle').width()});
